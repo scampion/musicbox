6385c94f10b161499427fc5748508a8a685c9bba
root <root@MusicBox.(none)>
2015-10-27 14:45:28 +0000

    stream

---
+++ streamuris.js
@@ -0,0 +1,11 @@
+/**
+ * Default streamuris which appear in the webinterface. Edit if you like.
+ * Take care when editting. Only edit the stuff between ''
+ * And don't use the default Windows Notepad for this (use Notepad++ on Windows)
+ */
+
+var streamUris = [];
+streamUris.push(["FIP", "http://audio.scdn.arkena.com/11016/fip-midfi128.mp3"]);
+streamUris.push(["Sing Sing", "http://www.sing-sing.org/confort2.pls"]);
+streamUris.push(["France Inter", "http://audio.scdn.arkena.com/11008/franceinter-midfi128.mp3"]);
+streamUris.push(["RFI", "http://rfi-monde-64k.scdn.arkena.com/rfimonde.mp3"]);
